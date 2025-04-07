0dd6c080328e18586963a9a89da72c96d84c74c3
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-07 15:01:17 +0200

    Pari e Dispari: check user input

--- Pari e Dispari/Js/main.js
+++ Pari e Dispari/Js/main.js
@@ -1,11 +1,19 @@
-const userChoice = prompt("Scegli se pari o dispari", "pari");
-const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5", 4));
+let userNumber;
+let userChoice;
+
+do {
+  userChoice = prompt("Scegli se pari o dispari", "pari");
+} while (userChoice !== "pari" && userChoice !== "dispari");
+
+do {
+  userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+} while (isNaN(userNumber) || userNumber < 1 || userNumber > 5);
 
 //Funzione che genera il numero random
-const pcNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-// const pcNumber = randomNumber(5);
-console.log("User Chioce: " + userChoice);
+// const pcNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+const pcNumber = randomNumber(5);
 
+console.log("User Chioce: " + userChoice);
 console.log("User Number: " + userNumber);
 console.log("PC Number: " + pcNumber);
 
@@ -13,9 +21,10 @@ const sumNumbers = userNumber + pcNumber;
 
 console.log("Sum: " + sumNumbers);
 
+// Funzione che definisce se il numero passato è pari
 const evenWin = userWin(sumNumbers);
 
-console.log("pari o dispari: " + evenWin);
+console.log("pari? " + evenWin);
 
 if ((userChoice === "pari") & evenWin) {
   console.log("Ha vinto l'utente");
@@ -26,7 +35,7 @@ if ((userChoice === "pari") & evenWin) {
 }
 
 /**
- *
+ * Questa funzione genera un numero casuale tra minimo e massimo che vengono passati
  * @param {number} max numero massimo del range
  * @param {number} min numero minimo del range
  * @returns {number} numero che viene generato
@@ -35,8 +44,9 @@ function randomNumber(max, min = 1) {
   const number = Math.floor(Math.random() * (max - min)) + min;
   return number;
 }
+
 /**
- *
+ * Questa funzione verifica se il numero è pari
  * @param {number} num la somma del numero dell'utente e del computer
  * @returns {boolean} isEvenWin ritorna True se il numero è pari e false se è dispari
  */
