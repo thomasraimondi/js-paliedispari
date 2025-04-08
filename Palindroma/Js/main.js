082fc16e0f89181fa64edb6e9d8eb7c1647cc991
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-08 09:48:28 +0200

    Palindromia: add function

--- Palindroma/Js/main.js
+++ Palindroma/Js/main.js
@@ -7,13 +7,16 @@ do {
 
 console.log("User Word: " + userWord);
 
-for (i = userWord.length - 1; i >= 0; i--) {
-  const currentValue = userWord[i];
-  reverseUserWord += currentValue;
+const isPalindroma = isWordPalindroma(userWord);
+if (isPalindroma) console.log("La parola è palindroma");
+else console.log("La parola non è palindroma");
+
+function isWordPalindroma(userWord) {
+  for (i = userWord.length - 1; i >= 0; i--) {
+    const currentValue = userWord[i];
+    reverseUserWord += currentValue;
+  }
+  console.log("Reverse User Word: " + reverseUserWord);
+  const isPalindroma = userWord === reverseUserWord ? true : false;
+  return isPalindroma;
 }
-
-console.log("Reverse User Word: " + reverseUserWord);
-
-const isPalindroma = userWord === reverseUserWord ? true : false;
-
-console.log("La parola è palindroma: " + isPalindroma);
